0fa67b358555760b26b65f007c7fa03f53453d64
Felipe Vieira <mrchampz@outlook.com>
2023-06-26 09:32:37 -0300

    terraform-bootstrap-eks-with-apps: added terraform code

---
+++ terraform-bootstrap-eks-with-apps/terraform/cluster/variables.tf
@@ -0,0 +1,25 @@
+variable "name" {
+  default = "eks-upgrades-workshop"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.35.0.0/16"
+}
+
+variable "cluster_version" {
+  default = "1.24"
+}
+
+variable "git_branch" {
+  default = "main"
+}
+
+variable "git_password" {}
+
+variable "git_username" {}
+
+variable "git_url" {}
